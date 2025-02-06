d4ce52e4f85b86aec933e742bffa8f84f6b022df
Jayantx07 <jayant.kumawat.s64@kalvium.community>
2025-02-06 10:05:19 +0530

    MongoDB connection setup successfully

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-const express = require('express');
-const { resolve } = require('path');
+  require("dotenv").config(); // Load environment variables
+  const express = require("express");
+  const mongoose = require("mongoose");
+  
+  const app = express();
+  const PORT = process.env.PORT || 5000;
+  const MONGO_URL = process.env.MONGO_URL;
 
-const app = express();
-const port = 3010;
+// Connect to MongoDB
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((error) => {
+    console.error("Error connecting to database:", error.message);
+  });
 
-app.use(express.static('static'));
+// Middleware
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.get("/", (req, res) => {
+  res.send("Express server is running");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
